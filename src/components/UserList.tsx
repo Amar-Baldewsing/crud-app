200c55a0c6fa91f723c6ebb44eb44859c0c34771
Amar Baldewsing <abaldewsing@qualogy.com>
2024-10-07 13:48:42 +0200

    minor changes UserList

--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -1,11 +1,18 @@
 import Users from '../db/Users.json';
 
 function UserList() {
+
     return(
         <main className="px-10 py-5">
-            <div id="container-main" className="flex flex-col tems-center= shadow-md border-2 rounded-lg p-4">
-                {Users.users.map((user) => {
-                    return (<div className='' key={user.id}>{user.id}| {user.naam}({user.leeftijd})</div>)
+            <h1 className='font-mono font-medium text-lg'>Alle Gebruikers</h1>
+            <div id="container-main" className="flex flex-col tems-center= shadow-md border-2 rounded-lg">
+                {Users.users.map((users) => {
+                    return (
+                        <div className="flex justify-between items-center p-3 border-solid border-2 border-slate-100">
+                            <p>{users.id}| {users.naam}</p>
+                            {/* <button className="font-sans text-sm rounded-lg bg-slate-600 font-normal text-white p-2">More info</button> */}
+                        </div>
+                    )
                 })}
             </div>
         </main>
